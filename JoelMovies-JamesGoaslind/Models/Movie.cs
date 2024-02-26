1b4dd1f8f6ad1a9ba0e7ede90601e9f63bafe117
jimy-goose <131339722+jimy-goose@users.noreply.github.com>
2024-02-26 08:57:00 -0700

    Nav

--- JoelMovies-JamesGoaslind/Models/Movie.cs
+++ JoelMovies-JamesGoaslind/Models/Movie.cs
@@ -16,6 +16,7 @@ namespace JoelMovies_JamesGoaslind.Models
         public int? CategoryId { get; set; }
         public Category? Category { get; set; }    
         public string Title { get; set; }
+
         [Required(ErrorMessage = "The Year field is required")] //Message when error
         [Range(1888, int.MaxValue, ErrorMessage = " Must be earlier than 1888")] 
         public int Year { get; set; }
